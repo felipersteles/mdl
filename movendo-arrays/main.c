47596b7adf6b4342349b725ce417d99f1387086b
Felipe Teles <felipersteles@gmail.com>
2023-04-27 12:42:47 -0300

    add: right stuffs

--- movendo-arrays/main.c
+++ movendo-arrays/main.c
@@ -6,6 +6,33 @@
 
 int main()
 {
+    drawTelesLogo();
+    printf("Estudos de um jogo de damas rs");
+
+    return 0;
+}
+
+// Implementação baseada em reuso
+// codigos tirados do repositorio:
+// https://github.com/felipersteles/Estrutura-de-Dados-1.git
+// =================================================================
+
+void escreveMatVet(int *matvet, int linha, int coluna)
+{
+
+    int i, j;
+
+    for (i = 0; i < linha; i++)
+    {
+        for (j = 0; j < coluna; j++)
+        {
+            scanf("%d", &matvet[(i * coluna) + j]);
+        }
+    }
+}
+
+void drawTelesLogo()
+{
     system("cls");
     system("@echo off");
     system("echo made by Felipe");
@@ -33,27 +60,4 @@ int main()
     system("echo ----------------------------------------------------------------");
     system("echo ------------------------------------------------------");
     system("pause");
-
-    printf("Estudos de um jogo de damas rs");
-
-    return 0;
-}
-
-// Implementação baseada em reuso
-// codigos tirados do repositorio:
-// https://github.com/felipersteles/Estrutura-de-Dados-1.git
-// =================================================================
-
-void escreveMatVet(int *matvet, int linha, int coluna)
-{
-
-    int i, j;
-
-    for (i = 0; i < linha; i++)
-    {
-        for (j = 0; j < coluna; j++)
-        {
-            scanf("%d", &matvet[(i * coluna) + j]);
-        }
-    }
 }
