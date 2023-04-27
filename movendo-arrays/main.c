e5215f48d317241c12fded9e4058b4ef41cff9c8
Felipe Teles <felipersteles@gmail.com>
2023-04-27 15:40:46 -0300

    add: conclusao

--- movendo-arrays/main.c
+++ movendo-arrays/main.c
@@ -1,45 +1,23 @@
 
 #include <stdio.h>
+#include <stdlib.h>
 
 // antes de codificiar eu queria dizer que psicografei tudo isso
 // em um papel, mas rlx... nao foi um papel qualquer, foi em um caderno rs
 
+int *cria_matrix(int linha, int coluna);
+void printa_matrix(int **mat, int linha, int coluna);
+void verificarMovimentos(int x, int y, int **mat, int linha, int coluna);
+
 int main()
 {
-    drawTelesLogo();
-    printf("Estudos de um jogo de damas rs");
-
-    return 0;
-}
-
-// Implementação baseada em reuso
-// codigos tirados do repositorio:
-// https://github.com/felipersteles/Estrutura-de-Dados-1.git
-// =================================================================
-
-void escreveMatVet(int *matvet, int linha, int coluna)
-{
-
-    int i, j;
-
-    for (i = 0; i < linha; i++)
-    {
-        for (j = 0; j < coluna; j++)
-        {
-            scanf("%d", &matvet[(i * coluna) + j]);
-        }
-    }
-}
-
-void drawTelesLogo()
-{
     system("cls");
-    system("@echo off");
+    system("echo ------------------------------------------------------");
+    system("echo ----------------------------------------------------------------");
     system("echo made by Felipe");
-    system("echo ------------------------------------------------------");
-    system("echo ----------------------------------------------------------------");
-    system("echo ----------------------------------------------------------------");
-    system("echo ------------------------------------------------------");
+    system("echo rs");
+    system("echo --------------------------------------------------");
+    system("echo ----------------------------------------------");
     system("echo @@@@@@@  @@@@@@@@  @@@       @@@@@@@@   @@@@@@");
     system("echo @@@@@@@  @@@@@@@@  @@@       @@@@@@@@  @@@@@@@");
     system("echo  @@!    @@!       @@!       @@!       !@@    ");
@@ -48,16 +26,105 @@ void drawTelesLogo()
     system("echo  !!!    !!!!!:    !!!       !!!!!:     !!@!!!");
     system("echo  !!:    !!:       !!:       !!:            !:!");
     system("echo  :!:    :!:       :!:       :!:           !:!");
-    system("echo  ::     :: ::::   :: ::::   :: ::::  :::: ::");
-    system("echo   :     : :: ::   : :: : :  : :: ::   :: : :");
+    system("echo   ::     :: ::::   :: ::::   :: ::::  :::: ::");
+    system("echo    :     : :: ::   : :: : :  : :: ::   :: : :");
+    system("echo ---------------------------------------------");
+    system("echo ----------------------------------------");
+
+    // inicio dos estudos
+    printf("Estudos de um jogo de damas rs");
+
+    // Todo: Primeiro: alocar uma matrix 5x5
+    int i, j, n = 6, m = 6, valor;
+
+    int **matrix = malloc(n * sizeof(int *));
+    for (i = 0; i < 5; i++)
+    {
+        matrix[i] = malloc(m * sizeof(int *));
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        for (j = 0; j < m; j++)
+        {
+            matrix[i][j] = 0;
+        }
+    }
+
+    matrix[4][1] = 1;
+    matrix[3][2] = 2;
+    matrix[1][4] = 2;
+
+    // todo: Segundo: Receber input
+    int x, y;
+
+    int menu = -1;
+    while (menu != 0)
+    {
+        printf("Menu:");
+        scanf("%d", &menu);
+
+        printa_matrix(matrix, n, m);
+        printf("\nSrc x e y: ");
+        scanf("%d%d", &x, &y);
+        verificarMovimentos(x, y, matrix, n, m);
+        printa_matrix(matrix, n, m);
+        break;
+    }
+
+    // todo: Alterando os valores conforme o funcionamento de um jogo de damas
+
+    // validar se memoria esta de fato alocada
+    // antes de desalocar
+    free(matrix);
+
+    system("@echo off");
+    system("echo .... My github: felipersteles .........");
     system("echo ------------------------------------------------------");
     system("echo ----------------------------------------------------------------");
-    system("echo ----------------------------------------------------------------");
-    system("echo ------------------------------------------------------");
-    system("echo My github: felipersteles");
-    system("echo ------------------------------------------------------");
-    system("echo ----------------------------------------------------------------");
-    system("echo ----------------------------------------------------------------");
-    system("echo ------------------------------------------------------");
-    system("pause");
+    return 0;
+}
+
+// Implementação baseada em reuso
+// codigos tirados do repositorio:
+// https://github.com/felipersteles/Estrutura-de-Dados-1.git
+// =================================================================
+
+void verificarMovimentos(int x, int y, int **mat, int linha, int coluna)
+{
+
+    printf("[%d][%d] = %d", x, y, mat[x][y]);
+    int valor;
+
+    if (mat[x][y] == 1)
+    {
+        if (mat[x - 1][y + 1] == 0)
+        {
+            printf("\npos possivel: [%d][%d] = %d", x - 1, y + 1, mat[x - 1][y + 1]);
+            mat[x - 1][y + 1] = 8;
+        }
+
+        if (mat[x - 1][y + 1] == 2 && mat[x - 2][y + 2] == 0)
+        {
+            printf("\n[%d][%d] = %d\n", x - 2, y + 2, mat[x - 2][y + 2]);
+            verificarMovimentos(x - 1, y + 1, mat, linha, coluna);
+        }
+    }
+
+    printa_matrix(mat, linha, coluna);
+}
+
+void printa_matrix(int **mat, int linha, int coluna)
+{
+    int i, j;
+
+    printf("\n");
+    for (i = 0; i < linha; i++)
+    {
+        for (j = 0; j < coluna; j++)
+        {
+            printf("| [%d][%d]%d | ", i, j, mat[i][j]);
+        }
+        printf("\n");
+    }
 }
